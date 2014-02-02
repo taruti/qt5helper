5e05335fa273fca46cd05c3044c9d84fb18e13f6
Taru Karttunen <taruti@taruti.net>
2014-02-02 18:38:00 +0200

    EnchangHighlighter

---
+++ enchanthighlighter.cc
@@ -0,0 +1,46 @@
+#include "enchanthighlighter.hh"
+#include <enchant++.h>
+#include <QDebug>
+
+EnchantHighlighter::EnchantHighlighter(QTextDocument *parent) :
+    QSyntaxHighlighter(parent)
+{
+    format.setUnderlineColor(QColor(Qt::red));
+    format.setUnderlineStyle(QTextCharFormat::SpellCheckUnderline);
+}
+
+EnchantHighlighter::~EnchantHighlighter() {}
+
+void EnchantHighlighter::check(const QString &text, int begin, int i) {
+    QStringRef sub(&text, begin, i-begin);
+    auto ba = sub.toUtf8();
+    if(!dict->check(std::string {ba.data(), (std::string::size_type)ba.size()}))
+        setFormat(begin, i-begin, format);
+}
+
+void EnchantHighlighter::highlightBlock(const QString& text) {
+    if(!dict)
+        return;
+    int begin = 0;
+    int i = 0;
+    for(; i<text.length(); ++i) {
+        auto ch = text.at(i);
+        if(ch < 'A') {
+            if(begin >= i)
+                begin++;
+            else if(begin<i) {
+                check(text, begin, i);
+                begin = i+1;
+            }
+        }
+    }
+    if(begin < i)
+        check(text, begin, i);
+}
+
+bool EnchantHighlighter::setLanguage(const std::string &lang) {
+    if(!enchant::Broker::instance()->dict_exists(lang))
+        return false;
+    dict.reset(enchant::Broker::instance()->request_dict(lang));
+    return true;
+}
---
+++ enchanthighlighter.hh
@@ -0,0 +1,27 @@
+#ifndef ENCHANTHIGHLIGHTER_HH
+#define ENCHANTHIGHLIGHTER_HH
+
+#include <memory>
+#include <QSyntaxHighlighter>
+#include <QTextCharFormat>
+
+typedef struct str_enchant_dict   EnchantDict;
+namespace enchant {
+    class Dict;
+}
+
+class EnchantHighlighter : public QSyntaxHighlighter
+{
+    Q_OBJECT
+    std::unique_ptr<enchant::Dict> dict;
+    QTextCharFormat format;
+    void check(const QString &ref, int, int);
+public:
+    explicit EnchantHighlighter(QTextDocument *parent = 0);
+    ~EnchantHighlighter();
+
+    bool setLanguage(const std::string &language);
+    virtual void highlightBlock(const QString &text) override;
+};
+
+#endif // ENCHANTHIGHLIGHTER_HH
